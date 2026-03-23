e213cfd59cccc7977279b3c5d6e38ad32038668d
nerdyboyy69 <nerdyboyy69@gmail.com>
2026-03-23 15:28:02 +0500

    aliexpress working on men's category beer's 319 pages done

--- instacart_scrape/main.py
+++ instacart_scrape/main.py
@@ -118,7 +118,7 @@ def get_all_pages(driver, wait, input_url, filename):
 
     current_page = input_url
 
-    max_pages = 70
+    max_pages = 12
     page_count = 0
 
     while page_count < max_pages:
